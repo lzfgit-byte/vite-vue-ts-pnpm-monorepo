3b66bab23af3716eff8a3981bacdb1995ffd026b
lzfgit-byte <1807409742@qq.com>
2023-05-06 11:10:29 +0800

    init

--- packages/components/src/components/HelloWord/src/HelloWord.vue
+++ packages/components/src/components/HelloWord/src/HelloWord.vue
@@ -3,7 +3,7 @@
   <button @click="handlerClick">{{ `click:${count}` }}</button>
 </template>
 <script setup lang="ts">
-  import { ref } from '@vue/reactivity';
+  import { ref } from 'vue';
 
   const count = ref(0);
   const handlerClick = () => {
