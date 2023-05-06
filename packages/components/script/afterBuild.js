11183a2b14e2da74e45929daaa56a37d2451174d
lzfgit-byte <1807409742@qq.com>
2023-05-06 10:55:33 +0800

    init

--- packages/components/script/afterBuild.js
+++ packages/components/script/afterBuild.js
@@ -3,16 +3,17 @@ import { emptyDirSync, ensureDirSync, ensureFileSync } from 'fs-extra/esm';
 
 const pckPath = `${process.cwd()}/package.json`;
 
-const upVersion = () => {
+const moveTgz = () => {
   ensureFileSync(pckPath);
   const data = readFileSync(pckPath);
   const pckJson = JSON.parse(data);
+  const name = pckJson.name.replace('@', '').replace(/\//g, '-');
   const version = pckJson.version;
-  const tgzPath = `${process.cwd()}/power-frontend-${version}.tgz`;
+  const tgzPath = `${process.cwd()}/${name}-${version}.tgz`;
   ensureFileSync(tgzPath);
   const toPath = `${process.cwd()}/lib`;
   ensureDirSync(toPath);
   emptyDirSync(`${process.cwd()}/lib`);
-  renameSync(tgzPath, `${process.cwd()}/lib/power-frontend-${version}.tgz`);
+  renameSync(tgzPath, `${process.cwd()}/lib/${name}-${version}.tgz`);
 };
-upVersion();
+moveTgz();
