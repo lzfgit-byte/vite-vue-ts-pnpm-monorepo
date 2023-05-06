b5ac859b9e32be3396b3af8a7f2e8ac2c2ffbb5f
lzfgit-byte <1807409742@qq.com>
2023-05-06 10:34:28 +0800

    init

--- packages/components/src/components/HelloWord/src/HelloWord.vue
+++ packages/components/src/components/HelloWord/src/HelloWord.vue
@@ -1,7 +1,6 @@
 <template>
   <h1>Hello word!</h1>
   <button @click="handlerClick">{{ `click:${count}` }}</button>
-  {{ count }}
 </template>
 <script setup lang="ts">
   import { ref } from '@vue/reactivity';
--- packages/components/src/main.ts
+++ packages/components/src/main.ts
@@ -3,7 +3,6 @@ import ElementPlus from 'element-plus';
 import locale from 'element-plus/lib/locale/lang/zh-cn'; // element中文
 import App from '@/App.vue';
 import 'element-plus/dist/index.css';
-import { PFrontend } from '@/index';
 
 const app = createApp(App);
-app.use(ElementPlus, { locale }).use(PFrontend).mount('#app');
+app.use(ElementPlus, { locale }).mount('#app');
